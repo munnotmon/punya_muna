6e86ab6ebc13802c3e3b89d28a8359dca00acd49
maulidaprina <mldaprna@gmail.com>
2025-06-04 23:49:34 +0700

    lelucon

---
+++ halo.java
@@ -0,0 +1,16 @@
+public class halo {
+    public static void main(String[] args) {
+        System.out.println("Halo, teman! Aku di sini untuk menghiburmu!");
+        System.out.println("Kenapa ayam menyeberang jalan?");
+        System.out.println("Untuk pergi ke sisi yang lain! Haha!");
+
+        System.out.println("\nBagaimana dengan lelucon lainnya?");
+        System.out.println("Apa yang terjadi jika kamu melempar sebuah kunang-kunang ke laut?");
+        System.out.println("Dia akan menjadi 'kunang-kunang laut'! Haha!");
+
+        System.out.println("\nIngat, meskipun kamu merasa kesepian, aku selalu ada di sini untuk membuatmu tersenyum!");
+    }
+}
+
+//tessss
+// Ini adalah program Java sederhana yang memberikan semangat dan humor kepada teman yang merasa kesepian.
